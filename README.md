ac355be84e274d606056cc63e4fb1db24e655f2b
Neocky <Neocky@users.noreply.github.com>
2024-03-22 11:31:54 +0100

    Added better documentation

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
-# Cozy-Workshop
 
- A Minecraft Modpack for Forge 1.20.1
+# 🔥 Cozy Workshop
+A cozy minecraft modpack to play with your girlfriend.  
+This is an easy to grasp modpack with quests, farming and the mod Create to experience a seamless expansion and expand the minecraft horizon.  
+The modpack is pretty light with under 100 mods in total and only needs 4GB of ram on my shitty laptop to run with 60 FPS.  
+This modpack is suited for beginners and every mod in this pack is documented in-game and guided by quests.
 
 
-# Building
-
+## ⚙ Building
 1. Have [Prism Launcher](https://prismlauncher.org/) & [packwiz](https://packwiz.infra.link/) installed
 2. Clone repository
 3. Export repository with `packwiz modrinth export`
 4. Import `.mrpack`-File in Prism Launcher and use the same folder as the repository
 5. Update the Prism Instance when adding new mods with these steps: 
     1. Add new mods with `packwiz modrinth add MOD`
+    2. Do the same for the *mods_curseforge* folder with `packwiz curseforge add MOD`
     2. Export pack with `packwiz modrinth export` 
     3. Apply update in Prism Launcher with: Rightclick > Edit > Modrinth > Update from file
 
+
+### Export for Curseforge
+In the folder `mods_curseforge` exists a curseforge packwiz pack. Only the mods are present and every other folder needs to be copied.
+To simplify this process just run `build_pack.py` to export the curseforge pack with packwiz.  
+The packwiz.exe file needs to be present in the root folder!
+This script will copy all necessary files and export the resulting modpack with packwiz for curseforge. 
+
+
+### Export for Modrinth
+Run this packwiz command:
+````
+.\packwiz.exe curseforge export -o MODPACK_NAME.zip
+```
--- build_pack.py
+++ build_pack.py
@@ -6,10 +6,13 @@ import shutil
 
 
 # build curseforge version
-def build_modpack_curseforge(curseforge_folder: str) -> None:
+def build_modpack_curseforge(packname: str, curseforge_folder: str) -> None:
     """
     Builds the modpack for Curseforge
-    
+
+    :packname: Modpack name without file suffix
+    :curseforge_folder: Path to Modpack instance of curseforge mods
+
     :returns: None
     """
     if os.path.exists(f"{curseforge_folder}/config/"):
@@ -19,22 +22,18 @@ def build_modpack_curseforge(curseforge_folder: str) -> None:
             shutil.rmtree(f"{curseforge_folder}/kubejs/")
 
     # copy needed files to curseforge folder
-    try:
-        shutil.copy("packwiz.exe", f"{curseforge_folder}/")
-        shutil.copytree("config/", f"{curseforge_folder}/config/")
-        #shutil.copytree("defaultconfigs/", "mods_curseforge")
-        #shutil.copytree("fancymenud_data/", "mods_curseforge")
-        shutil.copytree("kubejs/", f"{curseforge_folder}/kubejs/")
-    except Exception as e:
-        print("Files couldn't be copied correctly!")
-        print(e)
-        exit()
+    shutil.copy("packwiz.exe", f"{curseforge_folder}/")
+    shutil.copytree("config/", f"{curseforge_folder}/config/")
+    #shutil.copytree("defaultconfigs/", f"{curseforge_folder}/defaultconfigs/")
+    #shutil.copytree("fancymenu_data/", f"{curseforge_folder}/fancymenu_data/")
+    shutil.copytree("kubejs/", f"{curseforge_folder}/kubejs/")
 
     os.chdir(curseforge_folder)
+
     # create export for curseforge
     try:
-        os.system("packwiz.exe curseforge export --output Cozy-Workshop[curseforge].zip")
-        shutil.copy("Cozy-Workshop[curseforge].zip", "../")
+        os.system(f"packwiz.exe curseforge export --output {packname}.zip")
+        shutil.copy(f"{packname}.zip", "../")
     except Exception as e:
         print("Curseforge pack couldn't be exported!")
         print(e)
@@ -46,13 +45,14 @@ def build_modpack_curseforge(curseforge_folder: str) -> None:
     try:
         shutil.rmtree(f"{curseforge_folder}/config/")
         shutil.rmtree(f"{curseforge_folder}/kubejs/")
-        os.remove(f"{curseforge_folder}/packwiz.exe")
-        os.remove(f"{curseforge_folder}/Cozy-Workshop[curseforge].zip")
+        #os.remove(f"{curseforge_folder}/packwiz.exe")
+        os.remove(f"{curseforge_folder}/{packname}.zip")
     except Exception as e:
         print(e)
         exit()
 
+    return
 
 
 if __name__ == "__main__":
-    build_modpack_curseforge("mods_curseforge")
+    build_modpack_curseforge("Cozy-Workshop[curseforge]", "mods_curseforge")
