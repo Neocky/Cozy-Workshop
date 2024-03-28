5d7e55818436011f20caeced62b6d55aab429831
Neocky <Neocky@users.noreply.github.com>
2024-03-28 21:58:35 +0100

    Added Kitchen Chapter

--- kubejs/client_scripts/jei_information.js
+++ kubejs/client_scripts/jei_information.js
@@ -1,4 +1,13 @@
 
-JEIEvents.information(event => {
-    event.addItem("minecraft:wheat_seeds", ["Wheat Seeds can be obtained from breaking Grass."])
+JEIEvents.information(e => {
+    e.addItem(
+        "minecraft:wheat_seeds", [
+            "Wheat Seeds can be obtained from breaking Grass."
+    ])
+
+    e.addItem(
+        "farmersdelight:skillet", [
+        "Skillets let you cook on the go. Stand near heat, then hold food in your other hand. Start cooking by holding right click.",
+        "Skillets can be placed down as a block while sneaking."
+    ])
 })
