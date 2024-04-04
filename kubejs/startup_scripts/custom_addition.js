09c486c98de2f8786dd5f5627e47d4c423e86dd6
Neocky <Neocky@users.noreply.github.com>
2024-04-04 14:09:08 +0200

    Added Chocolate Ore

---
+++ kubejs/startup_scripts/custom_addition.js
@@ -0,0 +1,33 @@
+
+/**
+ * @file Adds custom blocks etc.
+ * @author Neocky <https://github.com/Neocky>
+ */
+
+
+StartupEvents.registry('block', event => {
+    event.create('kubejs:chocolate_ore') // Create a new block
+      .displayName("Chocolate Ore")
+      .soundType('stone') // Set a material (affects the sounds and some properties)
+      .hardness(3.0)
+      .resistance(3.0)
+      .requiresTool(true) // Requires a tool or it won't drop (see tags below)
+      .tagBlock('forge:ores')
+      .tagBlock('forge:ores_in_ground/stone')
+      .tagBlock('balm:ores')
+      .tagBlock('minecraft:mineable/pickaxe') // or a pickaxe
+      .tagBlock('minecraft:needs_iron_tool') // the tool tier must be at least iron
+    
+
+    event.create('kubejs:deepslate_chocolate_ore') // Create a new block
+      .displayName("Deepslate Chocolate Ore")
+      .soundType('stone') // Set a material (affects the sounds and some properties)
+      .hardness(3.0)
+      .resistance(3.0)
+      .requiresTool(true) // Requires a tool or it won't drop (see tags below)
+      .tagBlock('forge:ores')
+      .tagBlock('forge:ores_in_ground/stone')
+      .tagBlock('balm:ores')
+      .tagBlock('minecraft:mineable/pickaxe') // or a pickaxe
+      .tagBlock('minecraft:needs_iron_tool') // the tool tier must be at least iron
+})
---
+++ kubejs/startup_scripts/custom_generation.js
@@ -0,0 +1,17 @@
+
+/*
+WorldgenEvents.add(event => {
+    event.addOre(ore => {
+        ore.id = 'kubejs:chocolate_ore' // (optional, but recommended) custom id for the feature
+        ore.biomes = {
+            not: {
+                category: "nether",
+            },
+        }
+        ore.addTarget('#minecraft:stone_ore_replaceables', 'kubejs:chocolate_ore')
+        ore.count([3, 10]).size(15).squared().triangleHeight(-64, 210);
+        ore.worldgenLayer = 'underground_ores'
+    })
+})
+
+*/
