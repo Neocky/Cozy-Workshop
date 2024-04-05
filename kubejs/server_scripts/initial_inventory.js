9c053b6a7e588d3a401cc1f2d4b32e1265d58814
Neocky <Neocky@users.noreply.github.com>
2024-04-05 19:36:45 +0200

    Added questbook to initial inventory

--- kubejs/server_scripts/initial_inventory.js
+++ kubejs/server_scripts/initial_inventory.js
@@ -5,6 +5,7 @@ PlayerEvents.loggedIn(event => {
     if (!event.player.persistentData.given_initial_Inventory)
     {
         event.player.persistentData.given_initial_Inventory = true
+        event.player.give(Item.of("ftbquests:book"))
         event.player.give(Item.of("minecraft:tropical_fish", {display:{Name:'{"translate":"mp.item.tropical_fish_huh.display", "italic":false, "color":"#F37021"}', Lore:['[{"translate":"mp.item.tropical_fish_huh.lore","italic":false, "color":"gray"}]']}}))
     }
 })
