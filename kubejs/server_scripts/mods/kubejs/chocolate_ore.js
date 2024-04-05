788335ac3f59cbd468877a969cf794e0f1272ba0
Neocky <Neocky@users.noreply.github.com>
2024-04-05 15:45:45 +0200

    Added recipes for Chocolate Ore & Explorer Compass

--- kubejs/server_scripts/mods/kubejs/chocolate_ore.js
+++ kubejs/server_scripts/mods/kubejs/chocolate_ore.js
@@ -5,71 +5,108 @@
  */
 
 LootJS.modifiers((event) => {
+
+    event.enableLogging();
+
     event
         .addBlockLootModifier("kubejs:chocolate_ore")
-        .dropExperience(2)
-            .when((c) => {
-                c.not((n) => {
-                    n.matchMainHand(ItemFilter.hasEnchantment("minecraft:silk_touch")
-        )})})
         .removeLoot(Ingredient.all)
-        .addLoot("create:bar_of_chocolate")
-            .withChance(50)
-            .not((n) => {
-                n.matchMainHand(ItemFilter.hasEnchantment("minecraft:silk_touch")
-        )})
-        .addLoot("kubejs:chococlate_ore")
-            .when((c) =>
-            c.matchMainHand(ItemFilter.hasEnchantment("minecraft:silk_touch"))
-        )
-        .addWeightedLoot(
-            [1, 4],
-            [
-                Item.of("monecraft:coal").withChance(35),
+        .pool( p => {
+            p.matchMainHand(ItemFilter.PICKAXE)
+            p.not( n => n.matchMainHand(ItemFilter.hasEnchantment("minecraft:silk_touch")))
+            p.dropExperience(2)
+            p.addWeightedLoot([1,4],
+            [ 
+                Item.of("minecraft:coal").withChance(35),
                 Item.of("minecraft:raw_iron").withChance(35),
-                Item.of("minecraft:raw_copper").withChance(25),
-                Item.of("create:raw_zinc").withChance(20),
+                Item.of("minecraft:raw_copper").withChance(20),
+                Item.of("create:raw_zinc").withChance(10),
                 Item.of("minecraft:raw_gold").withChance(10),
-                Item.of("minecraft:redstone").withChance(10),
+                Item.of("minecraft:redstone").withChance(5),
                 Item.of("minecraft:lapis_lazuli").withChance(5),
                 Item.of("minecraft:emerald").withChance(3),
                 Item.of("minecraft:diamond").withChance(1)
-            ]
-        ).not((n) => {
-            n.matchMainHand(ItemFilter.hasEnchantment("minecraft:silk_touch")
-        )});
+            ])
+            p.randomChance(0.33)
+            p.addLoot("create:bar_of_chocolate")
+            p.applyOreBonus("minecraft:fortune")
+        })
+        .pool( p => {
+            p.matchMainHand(ItemFilter.PICKAXE)
+            p.matchMainHand(ItemFilter.hasEnchantment("minecraft:silk_touch"))
+            p.addLoot("kubejs:chocolate_ore");
+        });
 
 
     event
         .addBlockLootModifier("kubejs:deepslate_chocolate_ore")
-        .dropExperience(2)
-        .removeLoot("kubejs:deepslate_chocolate_ore")
-        .addLoot("create:bar_of_chocolate").withChance(50)
-        .addWeightedLoot(
-            [1, 4],
-            [
-                Item.of("monecraft:coal").withChance(35),
+        .removeLoot(Ingredient.all)
+        .pool( p => {
+            p.matchMainHand(ItemFilter.PICKAXE)
+            p.not( n => n.matchMainHand(ItemFilter.hasEnchantment("minecraft:silk_touch")))
+            p.dropExperience(2)
+            p.addWeightedLoot([1,4],
+            [ 
+                Item.of("minecraft:coal").withChance(35),
                 Item.of("minecraft:raw_iron").withChance(35),
-                Item.of("minecraft:raw_copper").withChance(25),
-                Item.of("create:raw_zinc").withChance(20),
+                Item.of("minecraft:raw_copper").withChance(20),
+                Item.of("create:raw_zinc").withChance(10),
                 Item.of("minecraft:raw_gold").withChance(10),
-                Item.of("minecraft:redstone").withChance(10),
+                Item.of("minecraft:redstone").withChance(5),
                 Item.of("minecraft:lapis_lazuli").withChance(5),
                 Item.of("minecraft:emerald").withChance(3),
                 Item.of("minecraft:diamond").withChance(1)
-            ]
-        );
-});
+            ])
+            p.randomChance(0.33)
+            p.addLoot("create:bar_of_chocolate")
+            p.applyOreBonus("minecraft:fortune")
+        })
+        .pool( p => {
+            p.matchMainHand(ItemFilter.PICKAXE)
+            p.matchMainHand(ItemFilter.hasEnchantment("minecraft:silk_touch"))
+            p.addLoot("kubejs:deepslate_chocolate_ore");
+        });
+
+})
 
 
 
 
 ServerEvents.recipes(event => {
-    event.smelting('create:bar_of_chocolate', 'kubejs:chocolate_ore')
-    event.smelting('create:bar_of_chocolate', 'kubejs:deepslate_chocolate_ore')
+    event.smelting("create:bar_of_chocolate", "kubejs:chocolate_ore")
+    event.smelting("create:bar_of_chocolate", "kubejs:deepslate_chocolate_ore")
 
-    event.blasting('create:bar_of_chocolate', 'kubejs:chocolate_ore')
-    event.blasting('create:bar_of_chocolate', 'kubejs:deepslate_chocolate_ore')
+    event.blasting("create:bar_of_chocolate", "kubejs:deepslate_chocolate_ore")
+    event.blasting("create:bar_of_chocolate", "kubejs:chocolate_ore")
+
+
+    event.recipes.create.crushing([
+        Item.of("create:bar_of_chocolate"),
+        Item.of("minecraft:coal").withChance(0.35),
+        Item.of("2x create:crushed_raw_iron").withChance(0.35),
+        Item.of("2x create:crushed_raw_copper").withChance(0.20),
+        Item.of("2x create:crushed_raw_zinc").withChance(0.10),
+        Item.of("2x create:crushed_raw_gold").withChance(0.10),
+        Item.of("2x minecraft:redstone").withChance(0.05),
+        Item.of("2x minecraft:lapis_lazuli").withChance(0.05),
+        Item.of("minecraft:emerald").withChance(0.03),
+        Item.of("minecraft:diamond").withChance(0.01)
+        ], "kubejs:deepslate_chocolate_ore")
+
+
+    event.recipes.create.crushing([
+        Item.of("create:bar_of_chocolate"),
+        Item.of("minecraft:coal").withChance(0.35),
+        Item.of("2x create:crushed_raw_iron").withChance(0.35),
+        Item.of("2x create:crushed_raw_copper").withChance(0.20),
+        Item.of("2x create:crushed_raw_zinc").withChance(0.10),
+        Item.of("2x create:crushed_raw_gold").withChance(0.10),
+        Item.of("2x minecraft:redstone").withChance(0.05),
+        Item.of("2x minecraft:lapis_lazuli").withChance(0.05),
+        Item.of("minecraft:emerald").withChance(0.03),
+        Item.of("minecraft:diamond").withChance(0.01)
+        ], "kubejs:chocolate_ore")
+
 
 })
 
