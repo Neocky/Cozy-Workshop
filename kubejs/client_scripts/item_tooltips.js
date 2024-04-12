b1bcaca2d4fb2cf5150a46b960747664a7cd74de
Neocky <Neocky@users.noreply.github.com>
2024-04-12 13:29:50 +0200

    Added mods

---
+++ kubejs/client_scripts/item_tooltips.js
@@ -0,0 +1,6 @@
+
+
+
+ItemEvents.tooltip(event => {
+    event.add("balancedflight:ascended_flight_ring", "An incredibly dense ring.", "Despite its weight it allows you to fly everywhere (Angel Ring).", "Allows both Creative and enhanced Elytra flight.")
+})
